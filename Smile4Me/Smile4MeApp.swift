38a39c92aa78d8ef6819e8da4637c2531289f47f
superkookai <weerawut@live.com>
2025-12-22 17:28:02 +0700

    Initial Commit

---
+++ Smile4Me/Smile4MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smile4MeApp.swift
+//  Smile4Me
+//
+//  Created by Weerawut on 22/12/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct Smile4MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
